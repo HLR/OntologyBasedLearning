69982dc8f4a00c8595500f3184b4cde8e18bd118
Andrzej Uszok <auszok@ihmc.us>
2019-01-28 13:03:26 -0600

    Updated ontology and python query

--- src/olwManagment.py
+++ src/olwManagment.py
@@ -2,7 +2,7 @@ from owlready2 import *
 
 onto_path.append("../ontology")
 
-mySaulSpatialOnto = get_ontology("http://ontology.ihmc.us/Spatial/SaulSpatial.owl")
+mySaulSpatialOnto = get_ontology("http://ontology.ihmc.us/Spatial/SaulSpatialConfiguration.owl")
 mySaulSpatialOnto.load()
 
 print('Classes defined in SaulSpatial ontology:')
@@ -24,19 +24,23 @@ print('List defined spatialConfigurations:\n')
 for instance in mySaulSpatialOnto.spatialConfiguration.instances(): 
     print("  ", instance)
     
-    queryString1 = "SELECT ?p WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?p .}"
+    # get trajector for the current spatial configuration
+    queryString1 = "SELECT ?tr WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?tr .}"
 #   print(queryString1)
     print("    Found trajector", list(graph.query_owlready(queryString1)))
     
-    queryString2 = "SELECT ?col WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?p . " + "?p <" +  mySaulSpatialOnto.col.iri + "> ?col .}" 
+    # get color for the current trajector
+    queryString2 = "SELECT ?col WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?tr . " + "?tr <" +  mySaulSpatialOnto.hasSpatialEntity.iri + "> ?se ."  + "?se <" +  mySaulSpatialOnto.col.iri + "> ?col .}" 
 #   print(queryString2)
     print("    Found color of the trajector", list(graph.query_owlready(queryString2)), "\n")
     
-    queryString3 = "SELECT ?spanText WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?p . " + "?p <" +  mySaulSpatialOnto.hasSpan.iri + "> ?span ."  + "?span <" + mySaulSpatialOnto.hasSubText.iri + "> ?spanText .}"
+    # get span text for the trajector
+    queryString3 = "SELECT ?spanText WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasTr.iri + "> ?tr . " + "?tr <" +  mySaulSpatialOnto.hasSpatialEntity.iri + "> ?se ."  + "?se <" +  mySaulSpatialOnto.hasSpan.iri + "> ?span ."  + "?span <" + mySaulSpatialOnto.hasSubText.iri + "> ?spanText .}"
 #   print(queryString3)
     foundspans = list(graph.query_owlready(queryString3));
     print("    Found span text of the trajector", foundspans, "\n")
     
+    # get motion indicator for the current spatial configuration
     queryString4 = "SELECT ?p WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasM.iri + "> ?p .}"
 #   print(queryString4)
     print("    Found motion indicator", list(graph.query_owlready(queryString4)), "\n")
