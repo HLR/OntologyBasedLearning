86634f0f3d81e3dd3eaaa60f251b450366a38a9c
Andrzej Uszok <auszok@ihmc.us>
2019-01-29 23:44:03 -0600

    Updated ontology, changed structure of folders updated python script printing information from ontology

--- src/olwManagment.py
+++ src/olwManagment.py
@@ -1,21 +1,67 @@
+import os
 from owlready2 import *
+dash = '-' * 120
+
+def print_concepts(concepts, concepts_type_name):
+    spatial_concepts = list([currentConcept.name for currentConcept in concepts ])
+    spatial_concepts.sort(key=str.lower)
+    ont_batches = [spatial_concepts[i:i+4] for i in range(0, len(spatial_concepts), 4)]
+
+    print(dash)
+    tableTitle = concepts_type_name + " defined in SaulSpatial ontology"
+    print('{:^120}'.format(tableTitle))
+    print(dash)
+
+    for batch in ont_batches :
+        if len(batch) == 4:
+            print(f'{batch[0]:30} {batch[1]:30} {batch[2]:30} {batch[3]:30}')
+        elif len(batch) == 3:
+            print(f'{batch[0]:30} {batch[1]:30} {batch[2]:30}')
+        elif len(batch) == 2:
+            print(f'{batch[0]:30} {batch[1]:30}')
+        elif len(batch) == 1:
+            print(f'{batch[0]:30}')
+    print("\n")
 
 onto_path.append("../ontology")
 
+# Load Spatial Configuration ontology
 mySaulSpatialOnto = get_ontology("http://ontology.ihmc.us/Spatial/SaulSpatialConfiguration.owl")
 mySaulSpatialOnto.load()
 
-print('Classes defined in SaulSpatial ontology:')
-print(list(mySaulSpatialOnto.classes()))
-print("\n")
+# Get ontology classes defined in this ontology and print them in the table
+spatial_classes = list(mySaulSpatialOnto.classes())
+print_concepts(spatial_classes, "Classes")
 
-print('Properties defined in SaulSpatial ontology:')
-print(list(mySaulSpatialOnto.object_properties()))
-print("\n")
+# Get ontology properties defined in the ontology
+spatial_properties = list(mySaulSpatialOnto.object_properties())
+spatial_properties.extend(list(mySaulSpatialOnto.data_properties()))
+print_concepts(spatial_properties, "Properties")
+
+# Get individuals defined in this ontology and print them in the table
+spatial_individuals = list(mySaulSpatialOnto.individuals())
+print_concepts(spatial_individuals, "Individuals")
 
 # Load SaulSpatial Examples
-mySaulSpatialExamplesOnto = get_ontology("http://ontology.ihmc.us/Spatial/SaulSpatialExamples.owl")
-mySaulSpatialExamplesOnto.load()
+onto_path.append("../ontology/data")
+data_ontologies = []
+
+print("Found ontology data files: ",)        
+for filename in os.listdir("../ontology/data") :
+    if not filename.endswith(".owl") :
+        continue
+    
+    filePath = os.path.join("../ontology/data", filename)
+    print("   " + filename)
+    
+    f = open(filePath, "r")
+    for line in f.readlines() :
+        for item in line.split() :
+            if item.startswith("xml:base") :
+                indexesOfDoubleQuote = ( [pos for pos, char in enumerate(item) if char =='\"'] )
+                ontology_name = item[indexesOfDoubleQuote[0]+1:indexesOfDoubleQuote[1]]
+                data_ontologies.append(get_ontology(ontology_name).load())
+print("\n")
 
 graph = default_world.as_rdflib_graph()
 
@@ -43,5 +89,4 @@ for instance in mySaulSpatialOnto.spatialConfiguration.instances():
     # get motion indicator for the current spatial configuration
     queryString4 = "SELECT ?p WHERE {<" + instance.iri + "> <" +  mySaulSpatialOnto.hasM.iri + "> ?p .}"
 #   print(queryString4)
-    print("    Found motion indicator", list(graph.query_owlready(queryString4)), "\n")
-   
+    print("    Found motion indicator", list(graph.query_owlready(queryString4)), "\n")  
